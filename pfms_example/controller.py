106164ad4dd04aa1d4d79bd5b75b0fd1cb2d0c19
hmtmcse <hmtm.cse@gmail.com>
2021-03-01 15:48:50 +0600

    adjust some parameter

--- pfms_example/controller.py
+++ pfms_example/controller.py
@@ -1,11 +1,12 @@
 from flask import Blueprint
-from pfms.swagger.pfms_swagger_decorator import pfms_create
+from pfms.swagger.pfms_swagger_decorator import pfms_create, pfms_details, pfms_delete
 from pfms_example.dto.person_create_dto import PersonCreateDto
 from pfms_example.dto.person_details_dto import PersonDetailsDto
 from pfms_example.service import PersonService
 
 pfms_example = Blueprint("pfms_example", __name__, url_prefix="/api/v1/example")
 person_service = PersonService()
+
 
 @pfms_example.route("/create", methods=['POST'])
 @pfms_create(request_body=PersonCreateDto, response_obj=PersonDetailsDto)
@@ -14,6 +15,7 @@ def create():
 
 
 @pfms_example.route("/update/<int:id>", methods=['POST'])
+@pfms_create(request_body=PersonCreateDto, response_obj=PersonDetailsDto)
 def update(id:int):
     pass
 
@@ -24,13 +26,15 @@ def list():
 
 
 @pfms_example.route("/details/<int:id>", methods=['GET'])
+@pfms_details(response_obj=PersonDetailsDto)
 def details(id:int):
-    pass
+    return person_service.details(id)
 
 
 @pfms_example.route("/delete/<int:id>", methods=['DELETE'])
+@pfms_delete()
 def delete(id:int):
-    pass
+    return person_service.delete(id)
 
 
 
